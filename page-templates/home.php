c84262f75617abf206d0f18674af6b0044e6fe58
Ash - SIlverless <ash@silverless.co.uk>
2019-02-26 13:43:46 +0000

    Update - updated from staging

--- page-templates/home.php
+++ page-templates/home.php
@@ -361,8 +361,56 @@ endif;?>
 
 <!-- ******************* Gallery END ******************* -->
 
+<!-- ******************* News Section ******************* -->
+
+<h3 class="heading heading__lg mb1">Latest News</h3>   
+
+    <div class="post-summary">
+            
+            <?php 
+            $args = array (
+            	'post_type'              => 'post',
+            	'posts_per_page'         => '1'
+            );
+            
+            // The Query
+            $query = new WP_Query( $args );
+            
+            // The Loop
+            if ( $query->have_posts() ) {
+            	while ( $query->have_posts() ) {
+            		$query->the_post();
+            
+            $postImage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+     
+            <div class="post-summary__item">
+         
+                <a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>" class="post-image"><img src="<?php echo $postImage[0];?>"/></a>         
+                
+                <h2 id="post-<?php the_ID(); ?>" class="heading heading__md">
+        
+                    <a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                
+                </h2>
+             
+                <p class="heading heading__xs heading__alt-color font200 mb1"><?php the_date() ?></p> 
+                <?php the_excerpt() ?>     
+                
+                <a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>" class="">Read More</a>     
+      
+            </div>
+
+            <?php 	}
+            } else {
+            	// no posts found
+            }
+            // Restore original Post Data
+            wp_reset_postdata();?>
+
+        </div>
+
+<!-- ******************* News Section END ******************* -->
+
 </div><!--c-->
-
-
 
 <?php get_footer();?>
