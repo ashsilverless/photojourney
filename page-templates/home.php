745307ecb09cc40fa42032df5b81d8de948b221c
Ash - SIlverless <ash@silverless.co.uk>
2020-06-18 09:49:41 +0100

    no message

--- page-templates/home.php
+++ page-templates/home.php
@@ -164,12 +164,12 @@ get_header();?>
                 <div class="row">
 
                     <div class="col-md-6 col-3">
-                        <?php if(the_field('site_version', 'options')=='nepal'){
+                        <?php if(get_field('site_version', 'options')=='nepal'){
                             get_template_part( 'template-parts/nepal', 'map');
-                        } elseif(the_field('site_version', 'options')=='scotland') {
+                        } elseif(get_field('site_version', 'options')=='scotland') {
                             get_template_part( 'template-parts/scotland', 'map');
                         } else {
-                            
+                            //No map served
                         }?>
                     </div>
 
