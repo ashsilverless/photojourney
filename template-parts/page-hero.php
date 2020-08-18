3a20d5fa91ceaafcb7e9f74a65ea768aca4398b1
Ash - SIlverless <ash@silverless.co.uk>
2020-08-18 12:06:50 +0100

    DEV - add dark text switch

--- template-parts/page-hero.php
+++ template-parts/page-hero.php
@@ -2,23 +2,23 @@
 
 <?php $heroImage = get_field('background_image');?>
 
-<div class="wrapper-hero mb3" style="background-image: url(<?php echo $heroImage['url']; ?>);">
+<div class="wrapper-hero mb3 <?php if (get_field('dark_text') == 1){ echo 'dark-text'; }?>" style="background-image: url(<?php echo $heroImage['url']; ?>);">
 
     <div class="container">
-    
+
         <div class="row">
-                
-            <div class="col-12 wrapper-hero__content text-center">       
-                
-                <h1 class="heading heading__xl heading__light font800"><?php the_field( 'hero_heading' );?></h1>            
-                <h2 class="heading heading__sm heading__light font300"><?php the_field( 'copy' );?></h1>               
-            </div>       
-                
+
+            <div class="col-12 wrapper-hero__content text-center">
+
+                <h1 class="heading heading__xl heading__light font800"><?php the_field( 'hero_heading' );?></h1>
+                <h2 class="heading heading__sm heading__light font300"><?php the_field( 'copy' );?></h1>
+            </div>
+
         </div>
-    
+
     </div>
 
-    
+
 </div>
 
 <!-- ******************* Hero Content END ******************* -->
