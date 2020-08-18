f3319b91373073fd2e6ec2c4f5efda21d66b6efb
Ash - SIlverless <ash@silverless.co.uk>
2020-08-18 11:06:52 +0100

    DEV - incorporate child theme additions

--- header.php
+++ header.php
@@ -11,40 +11,47 @@
 
 <head>
 
- 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Guided Photography Tours, Nepal">
     <meta name="keywords" content=" ">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 
-	<title>Photo Journey | Guided Photography Tours</title>
+    <title>Photo Journey | Guided Photography Tours</title>
 
     <link rel="stylesheet" href="https://use.typekit.net/qnr3dic.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="76x76"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-16x16.png">
     <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>//site.webmanifest">
     <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>//safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
+    <script src="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js"></script>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css" rel="stylesheet" />
 
-
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 <?php $typeField = get_field('site_version', 'options');?>
+
 <body <?php body_class($typeField); ?>>
 
-	<div id="page" class="site-wrapper">
+    <div id="page" class="site-wrapper">
 
-		<nav>
+        <nav>
 
             <div class="nav-menu">
 
-            <?php
+                <?php
             wp_nav_menu( array(
             'theme_location' => 'main-menu',
             'container_class' => 'mainMenu' ) );
@@ -52,46 +59,46 @@
 
             </div>
 
-			<div class="container">
+            <div class="container">
 
-				<div class="row">
+                <div class="row">
 
-					<div class="col-sm-3 col-1">
+                    <div class="col-sm-3 col-1">
 
-						<div class="menu-trigger hamburger hamburger--collapse">
+                        <div class="menu-trigger hamburger hamburger--collapse">
 
-							<div class="hamburger-box">
+                            <div class="hamburger-box">
 
-								<div class="hamburger-inner"></div>
+                                <div class="hamburger-inner"></div>
 
-							</div>
+                            </div>
 
-						</div>
-
-					</div>
-
-					<div class="col-sm-6 col-10 brand">
-
-    				    <?php $brandImage = get_field('logo', 'options');?>
-
-						<a href="<?php echo home_url(); ?>" alt="<?php wp_title(''); ?>" title="<?php wp_title(''); ?>">
-
-    						<img src="<?php echo $brandImage['url'];?>" alt="" title=""/>
-
-						</a>
-
-					</div>
-
-                    <div class="col-sm-3 d-sm-block d-none book-now">
-
-                        <a href="/book-now">Book Now</a>
+                        </div>
 
                     </div>
 
-				</div>
+                    <div class="col-sm-6 col-10 brand">
 
-			</div>
+                        <?php $brandImage = get_field('logo', 'options');?>
 
-		</nav>
+                        <a href="<?php echo home_url(); ?>" alt="<?php wp_title(''); ?>" title="<?php wp_title(''); ?>">
 
-	<main>
+                            <img src="<?php echo $brandImage['url'];?>" alt="" title="" />
+
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-3 d-sm-block d-none book-now">
+
+                        <a href="/scotland/book-now">Book Now</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </nav>
+
+        <main>
--- page-templates/country-page.php
+++ page-templates/country-page.php
@@ -9,12 +9,12 @@ get_header();?>
 <?php get_template_part('template-parts/page', 'hero');?>
 
 <div class="container">
-    
-<?php get_template_part('template-parts/text', 'area');?>
 
-<!-- ******************* Main Content Area ******************* -->
+    <?php get_template_part('template-parts/text', 'area');?>
 
-<?php
+    <!-- ******************* Main Content Area ******************* -->
+
+    <?php
 
 // check if the flexible content field has rows of data
 if( have_rows('body_content') ):
@@ -24,51 +24,55 @@ if( have_rows('body_content') ):
 
         if( get_row_layout() == 'call_to_action' ):?>
 
-</div><!--c-->
+</div>
+<!--c-->
 
-    <div class="inline-cta mb3">
-        
-        <div class="container">
-            
-        	<h2 class="heading heading__md heading__alt-color"><?php the_sub_field('cta_heading');?></h2>
-        	
-            <a href="<?php the_sub_field('cta_button_target');?>" type="button" class="button button__inline-cta">
-            
-                <?php the_sub_field('cta_button_text');?><i class="fas fa-angle-right"></i>
-            
-            </a>
-       	        	
-        </div><!--c-->
+<div class="inline-cta mb3">
+
+    <div class="container">
+
+        <h2 class="heading heading__md heading__alt-color"><?php the_sub_field('cta_heading');?></h2>
+
+        <a href="<?php the_sub_field('cta_button_target');?>" type="button" class="button button__inline-cta">
+
+            <?php the_sub_field('cta_button_text');?><i class="fas fa-angle-right"></i>
+
+        </a>
+
+    </div>
+    <!--c-->
+
+</div>
+
+<div class="container">
+
+    <?php elseif( get_row_layout() == 'content_block' ): ?>
+
+    <div class="row mb3">
+
+        <div class="col-sm-6 mb2">
+
+            <img src="<?php the_sub_field('image');?>" />
+
+        </div>
+
+
+        <div class="col-sm-6">
+
+            <h2 class="heading heading__lg mt0"><?php the_sub_field('heading');?></h2>
+
+            <?php the_sub_field('copy');?>
+
+        </div>
 
     </div>
 
-<div class="container">
-        	        	
-        <?php elseif( get_row_layout() == 'content_block' ): ?>
-
-        <div class="row mb3">
- 
-            <div class="col-sm-6 mb2">              
-                
-                <img src="<?php the_sub_field('image');?>"/>
-            
-            </div>
- 
-             
-            <div class="col-sm-6">              
-                
-                <h2 class="heading heading__lg mt0"><?php the_sub_field('heading');?></h2>
-                
-                <?php the_sub_field('copy');?>
-            
-            </div>
-        
-        </div>
-
-        <?php endif;
+    <?php endif;
             endwhile;
             endif;?>
 
-</div><!--c-->
+</div>
+
+<!--c-->
 
 <?php get_footer();?>
